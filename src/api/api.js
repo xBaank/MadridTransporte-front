9f82403707d3321dff63cad53e4f08352b19885a
Roberto Blázquez <elroby444@gmail.com>
2023-04-17 17:11:50 +0200

    Allow .env.development.local (#3)
    Co-authored-by: Roberto <roberto.blazquez@cloudappi.net>

--- src/api/api.js
+++ src/api/api.js
@@ -1,22 +1,25 @@
-const apiUrl = 'http://localhost:8080/v1'
+const apiUrl = process.env.REACT_APP_BACK_URL;
 
 export async function getStopsTimesByCode(code) {
-    const response = await fetch(`${apiUrl}/stops/${code}/times`)
-    if (!response.ok) return response.status
-    const data = await response.json()
-    return data
+  console.log(process.env.NODE_ENV);
+  console.log(process.env);
+  console.log(apiUrl);
+  const response = await fetch(`${apiUrl}/stops/${code}/times`);
+  if (!response.ok) return response.status;
+  const data = await response.json();
+  return data;
 }
 
 export async function getLineLocations(code) {
-    const response = await fetch(`${apiUrl}/lines/${code}/mode/8/locations`)
-    if (!response.ok) return response.status
-    const data = await response.json()
-    return data
+  const response = await fetch(`${apiUrl}/lines/${code}/mode/8/locations`);
+  if (!response.ok) return response.status;
+  const data = await response.json();
+  return data;
 }
 
 export async function getItinerariesByCode(code) {
-    const response = await fetch(`${apiUrl}/lines/${code}/mode/8/itineraries`)
-    if (!response.ok) return response.status
-    const data = await response.json()
-    return data
+  const response = await fetch(`${apiUrl}/lines/${code}/mode/8/itineraries`);
+  if (!response.ok) return response.status;
+  const data = await response.json();
+  return data;
 }
