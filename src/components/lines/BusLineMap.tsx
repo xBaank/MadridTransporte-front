ba21cd10e1ba42bd305ec6271766fd4cc2373c10
Roberto Blázquez <elroby444@gmail.com>
2023-05-10 23:20:58 +0200

    Change maps loading

--- src/components/lines/BusLineMap.tsx
+++ src/components/lines/BusLineMap.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react";
-import { GoogleMap, KmlLayer, LoadScript, Marker } from '@react-google-maps/api';
+import { GoogleMap, KmlLayer, LoadScript, Marker, useJsApiLoader } from '@react-google-maps/api';
 import { getLineLocations, getItinerariesByCode } from "../../api/api";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -14,6 +14,12 @@ type location = {
 }
 
 export default function BusLineMap() {
+
+    const { isLoaded, loadError } = useJsApiLoader({
+        googleMapsApiKey: "" // ,
+        // ...otherOptions
+    })
+
 
     let firstLoad = useRef(true);
     const timeout = 20000;
@@ -56,7 +62,10 @@ export default function BusLineMap() {
     }, []);
 
     const ifLoading = () => {
-        if (loading) return <div className=' text-black text-center font-bold text-2xl'>Loading...</div>
+        if (loading || !isLoaded) return <div className=' text-black text-center font-bold text-2xl'>Loading...</div>
+        if (loadError) {
+            return <div>Map cannot be loaded right now, sorry.</div>
+        }
         else return map()
     }
 
@@ -73,40 +82,36 @@ export default function BusLineMap() {
     const map = () => {
         return (
             <div style={{ height: '100vh', width: '100%' }}>
-                <LoadScript
-                    googleMapsApiKey=""
+                <GoogleMap
+                    mapContainerStyle={containerStyle}
+                    center={currentPosition ?? new google.maps.LatLng(locations[0].coordinates.latitude, locations[0].coordinates.longitude)}
+                    zoom={10}
                 >
-                    <GoogleMap
-                        mapContainerStyle={containerStyle}
-                        center={currentPosition ?? new google.maps.LatLng(locations[0].coordinates.latitude, locations[0].coordinates.longitude)}
-                        zoom={10}
-                    >
-                        {
-                            <Fragment>
-                                {currentPostitionMarker()}
-                                <KmlLayer
-                                    url={itineraries[0].kml}
-                                    options={{ preserveViewport: true }}
+                    {
+                        <Fragment>
+                            {currentPostitionMarker()}
+                            <KmlLayer
+                                url={itineraries[0].kml}
+                                options={{ preserveViewport: true }}
 
-                                />
+                            />
 
-                                {
-                                    locations.map((location: { coordinates: { latitude: any; longitude: any; }; codVehicle: string | undefined; }) => {
-                                        return (
-                                            <Marker
-                                                position={{ lat: location.coordinates.latitude, lng: location.coordinates.longitude }}
-                                                title={location.codVehicle}
-                                                icon={{ url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Canberra_Bus_icon.svg/2048px-Canberra_Bus_icon.svg.png", scaledSize: { width: 30, height: 30, equals: () => true } }}
-                                            />
-                                        )
-                                    })
-                                }
+                            {
+                                locations.map((location: { coordinates: { latitude: any; longitude: any; }; codVehicle: string | undefined; }) => {
+                                    return (
+                                        <Marker
+                                            position={{ lat: location.coordinates.latitude, lng: location.coordinates.longitude }}
+                                            title={location.codVehicle}
+                                            icon={{ url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Canberra_Bus_icon.svg/2048px-Canberra_Bus_icon.svg.png", scaledSize: { width: 30, height: 30, equals: () => true } }}
+                                        />
+                                    )
+                                })
+                            }
 
-                            </Fragment>
-                        }
-                        <></>
-                    </GoogleMap>
-                </LoadScript>
+                        </Fragment>
+                    }
+                    <></>
+                </GoogleMap>
             </div>
         )
     }
