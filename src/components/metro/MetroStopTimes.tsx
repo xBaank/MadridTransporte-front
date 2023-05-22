12876e3a7b642001788b83985b3bf0402a5be94b
Roberto <robertobl022@gmail.com>
2023-05-22 13:40:39 +0200

    Added interface for metro

--- src/components/metro/MetroStopTimes.tsx
+++ src/components/metro/MetroStopTimes.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import _ from "lodash";
 import { useEffect, useState } from "react";
-import { getMetroTimes, getStopsTimesByCode, getStopsTimesByCodeCached } from "../../api/api";
+import { getMetroTimes, getMetroTimesById, getStopsTimesByCode, getStopsTimesByCodeCached } from "../../api/api";
 import { useParams } from "react-router-dom";
 
 export default function MetroStopsTimes() {
@@ -20,12 +20,13 @@ export default function MetroStopsTimes() {
     }
 
     useEffect(() => {
+        const load = (code: string) => getMetroTimesById(Number.parseInt(code));
         if (firstLoad.current) {
-            loadTimes(getMetroTimes);
+            loadTimes(load);
             firstLoad.current = false;
         }
         if (!firstLoad.current) {
-            const interval = setInterval(() => loadTimes(getStopsTimesByCode), timeout)
+            const interval = setInterval(() => loadTimes(load), timeout)
             return () => {
                 clearInterval(interval);
             }
@@ -56,15 +57,26 @@ export default function MetroStopsTimes() {
         return (
             <>
                 <div id="metroStops" className="grid grid-cols-1 mx-auto gap-4 max-w-4xl">
-                    {stops.map((value: any) => {
+                    <div className=" text-black text-center font-bold text-2xl">
+                        {stops.nombre_estacion}
+                    </div>
+                    {stops.times.map((value: any) => {
                         return (
                             <div className=" p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                                <div className="flex items-center justify-between">
-                                   { value.nombreli }
+                                <div className="flex items-center justify-between text-white">
+                                    Linea: {value.linea}
                                 </div>
-                                <div className="flex items-center justify-between">
-                                    Proximo en <span> { value.proximo } </span> minutos
+                                <div className="flex items-center justify-between text-white">
+                                    Anden: {value.anden}
                                 </div>
+                                {value.proximos.map((value: any) => {
+                                    return (
+                                        <div className=" text-white">
+                                            - {value} minutos
+                                        </div>
+                                    );
+                                })
+                                }
                             </div>
                         );
                     })}
