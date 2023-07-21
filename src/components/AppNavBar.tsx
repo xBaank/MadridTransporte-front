356f3b37291e2b82a1fe1fea1073b2ee65b7498e
Roberto Blázquez <elroby444@gmail.com>
2023-07-21 14:20:25 +0200

    Redefining UI

--- src/components/AppNavBar.tsx
+++ src/components/AppNavBar.tsx
@@ -5,64 +5,76 @@ import DirectionsTransitIcon from '@mui/icons-material/DirectionsTransit';
 import DirectionsBusIcon from '@mui/icons-material/DirectionsBus';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { IconButton, useTheme } from '@mui/material';
+import { Brightness4, Brightness7 } from '@mui/icons-material';
+import { ColorModeContext } from '..';
+
+
+function ThemeComponent() {
+    const theme = useTheme();
+    const colorMode = React.useContext(ColorModeContext);
+    return (
+        <IconButton onClick={colorMode.toggleColorMode} color="inherit">
+            {theme.palette.mode === 'dark' ? <Brightness7 /> : <Brightness4 />}
+        </IconButton>
+    );
+}
+
 
 export default function NavBar() {
     return (
-        <div className='flex flex-row bg-slate-800 p-4 border-b-4 border-purple-800 '>
-            <div className='mx-2 my-auto'>
+        <div className='flex flex-row p-4 border-b-4 border-purple-800 '>
+            <div className='mx-2 my-auto min-w-[8%]'>
                 <Link to={"/"}>
-                    <h2 className='text-2xl text-white font-bold align-middle'>Bus Tracker</h2>
+                    <h2 className='text-2xl  font-bold align-middle'>Bus Tracker</h2>
                 </Link>
             </div>
-            <div className='my-auto ml-2 border-l-2 border-purple-800'>
-                <ul className='flex flex-row ml-5 mr-2 my-auto border-b-1'>
-                    <li className='mr-5'>
+            <div className='my-auto ml-2 border-l-2 border-purple-800 w-full'>
+                <ul className='flex flex-row ml-5 mr-2 my-auto border-b-1 '>
+                    <li className='mr-5 mt-2'>
                         <Link to={"/"}>
-                            <div className='flex flex-row text-white  hover:text-purple-500'>
+                            <div className='flex flex-row  hover:text-purple-500'>
                                 <DirectionsBusIcon />
                                 <h3 className='ml-1' >Bus </h3>
                             </div>
                         </Link>
                     </li>
-                    <li className='mr-5'>
+                    <li className='mr-5 mt-2'>
                         <Link to={"/metro"}>
-                            <div className='flex flex-row text-white  hover:text-purple-500'>
+                            <div className='flex flex-row hover:text-purple-500'>
                                 <DirectionsTransitIcon />
                                 <h3 className='ml-1' >Metro </h3>
                             </div>
                         </Link>
                     </li>
-                    {
-                        !isLogged() ?
-                            <li className='mr-5'>
+
+                    <li className='mr-5 mt-2'>
+                        {
+                            !isLogged() ?
                                 <Link to={"/login"}>
-                                    <div className='flex flex-row text-white  hover:text-purple-500'>
+                                    <div className='flex flex-row hover:text-purple-500'>
                                         <LoginIcon />
                                         <h3 className='ml-1' >Login </h3>
                                     </div>
                                 </Link>
-                            </li>
-                            :
-                            <></>
-                    }
-                    {
-                        isLogged() ?
-                            <li className='mr-5'>
+                                :
                                 <Link to={"/"} onClick={
                                     () => {
                                         localStorage.removeItem('token')
                                         window.location.reload();
                                     }
                                 }>
-                                    <div className='flex flex-row text-white  hover:text-purple-500'>
+                                    <div className='flex flex-row hover:text-purple-500'>
                                         <LogoutIcon />
                                         <h3 className='ml-1' >Logout </h3>
                                     </div>
                                 </Link>
-                            </li>
-                            :
-                            <></>
-                    }
+                        }
+                    </li>
+
+                    <li className='ml-auto'>
+                        <ThemeComponent />
+                    </li>
                 </ul>
             </div>
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -15,6 +15,44 @@ import { Footer } from './components/Footer';
 import ResetPassword from './components/users/ResetPassword';
 import NewPassword from './components/users/NewPassword';
 import MetroStopsTimesId from './components/metro/MetroStopTimesId';
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
+
+export const ColorModeContext = React.createContext({ toggleColorMode: () => { } });
+
+
+export default function App() {
+  const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+  const colorMode = React.useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+
+
 
 const DefaultElement = (element: JSX.Element) => {
   return (
@@ -72,11 +110,8 @@ const router = createHashRouter([
 ]);
 
 const throwEx = () => { throw new Error("No root element found") }
-
 const root = ReactDOM.createRoot(document.getElementById('root') ?? throwEx());
-root.render(
-  <RouterProvider router={router} />
-);
+root.render(<App />);
 
 
 
