e5c29068e69f4b65aa68c54b4e18c2841a6c02aa
Roberto <robertobl022@gmail.com>
2023-08-15 16:17:53 +0200

    Remove unused code

--- src/components/metro/MetroStopTimesId.tsx
+++
@@ -1,8 +0,0 @@
-import { getMetroTimesById } from "../../api/api";
-import { useParams } from "react-router-dom";
-import MetroStopsTimes from "./MetroStopsTimes";
-
-export default function MetroStopsTimesId() {
-    const { id } = useParams<{ id: string }>();
-    return MetroStopsTimes(id ?? "", getMetroTimesById);
-}
