704543e2f115130c886c80fc419b2dbeffa0eb3c
Roberto <robertobl022@gmail.com>
2023-08-15 14:11:31 +0200

    Fix abono message

--- src/components/abono/api/Abono.ts
+++ src/components/abono/api/Abono.ts
@@ -4,7 +4,7 @@ import { AbonoType } from "./Types";
 
 export async function GetAbono(id: string) {
     const response = await fetch(`${apiUrl}/abono/${id}`);
-    if (!response.ok) return left("Error al obtener el abono");
+    if (!response.ok) return left((await response.json()).message);
     const data = await response.json() as AbonoType;
     return right(data);
 }
