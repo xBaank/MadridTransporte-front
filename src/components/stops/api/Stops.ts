6ac6c29a02c78f82e931c84307925207bf371d9c
Roberto <robertobl022@gmail.com>
2023-08-22 17:31:01 +0200

    Only download on fetch

--- src/components/stops/api/Stops.ts
+++ src/components/stops/api/Stops.ts
@@ -2,14 +2,14 @@ import { Either, left, right } from "fp-ts/lib/Either";
 import { Alert, Stop, TransportType } from "./Types";
 import { apiUrl } from "../../Urls";
 
-export let allStops: Either<string, Stop[]> | undefined
-getAllStops().then((stops) => allStops = stops)
+let allStops: Either<string, Stop[]> | undefined
 
 export async function getAllStops(): Promise<Either<string, Stop[]>> {
-    if(allStops !== undefined) return allStops;
+    if (allStops !== undefined) return allStops;
     const response = await fetch(`${apiUrl}/stops/all`);
     if (!response.ok) return left("Error al obtener las paradas");
     const data = await response.json() as Stop[];
+    allStops = right(data);
     return right(data);
 }
 
