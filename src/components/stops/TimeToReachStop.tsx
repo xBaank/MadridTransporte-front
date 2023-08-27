3c0fc27d00fd3087fe6120b52339c161449d7776
Roberto Blázquez <elroby444@gmail.com>
2023-08-28 01:37:44 +0200

    Better format to distance

--- src/components/stops/TimeToReachStop.tsx
+++ src/components/stops/TimeToReachStop.tsx
@@ -32,10 +32,17 @@ export default function TimeToReachStop({ stopLocation }: { stopLocation: Coordi
 
     const arrivesFormatted = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })
 
-    return <div className="flex justify-center">
-        <DirectionsWalkIcon />
-        <pre className={`font-bold text-center ${textColor}`}>
-            Llegada a la parada andando: {arrivesFormatted} ({(timeToReachStop.routes[0].distance / 1000).toFixed(2)} km)
-        </pre>
+    return <div className="flex flex-col justify-center">
+        <div className="flex justify-center">
+            <DirectionsWalkIcon />
+            <pre className={`font-bold text-center ${textColor}`}>
+                Llegada a la parada andando: {arrivesFormatted}
+            </pre>
+        </div>
+        <div>
+            <pre className={`font-bold text-center ${textColor}`}>
+                ({(timeToReachStop.routes[0].distance / 1000).toFixed(2)} km)
+            </pre>
+        </div>
     </div>
 }
