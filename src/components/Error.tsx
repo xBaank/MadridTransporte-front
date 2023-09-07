953929a2fddafad0252883baca048ed67ba3c117
Roberto Blázquez <elroby444@gmail.com>
2023-09-07 19:21:20 +0200

    Center Error

--- src/components/Error.tsx
+++ src/components/Error.tsx
@@ -3,7 +3,7 @@ import ErrorIcon from '@mui/icons-material/Error';
 
 export default function ErrorMessage({ message }: { message: string }) {
     return <>
-        <div className="flex font-bold text-lg flex-col justify-center items-center my-auto">
+        <div className="flex font-bold text-lg flex-col justify-center text-center items-center m-auto">
             <ErrorIcon className="text-red-500" fontSize="large" />
             <div className="text-red-500">{message}</div>
         </div>
