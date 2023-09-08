764735ba1a841ce2d3eeadfa41ac631f405d0b22
Roberto Blázquez <elroby444@gmail.com>
2023-09-09 01:42:38 +0200

    Change mobile size

--- src/components/DefaultElement.tsx
+++ src/components/DefaultElement.tsx
@@ -14,7 +14,7 @@ export default function DefaultElement({ element }: { element: JSX.Element }) {
 
 function RenderElement(element: JSX.Element) {
   const { width } = useWindowDimensions()
-  const isMobile = width < 418
+  const isMobile = width < 693
 
   return (
     <div className={`flex flex-col h-screen`}>
