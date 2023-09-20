277ff75ce2099bc5c10349013df1341760c6c6bb
Roberto Blázquez <elroby444@gmail.com>
2023-09-20 15:05:54 +0200

    Update

--- src/components/stops/api/Times.ts
+++ src/components/stops/api/Times.ts
@@ -8,7 +8,8 @@ const Error = "Error al obtener los tiempos de la parada seleccionada";
 export async function getStopsTimes(type: TransportType, code: string): Promise<Either<string, StopTimes>> {
     const response = await fetch(`${apiUrl}/stops/${type}/${code}/times`);
     if (response.status === 404) return left(NotFound);
-    if (!response.ok) return left(Error);
+    if (response.status === 400) return left(Error);
+    if (response.status === 500) return left(Error);
     const data = await response.json() as StopTimes;
     return right(data);
 }
@@ -16,6 +17,8 @@ export async function getStopsTimes(type: TransportType, code: string): Promise<
 export async function getTrainStopsTimes(originCode: string, destinationCode: string): Promise<Either<string, TrainStopTimes>> {
     const response = await fetch(`${apiUrl}/stops/train/times?originStopCode=${originCode}&destinationStopCode=${destinationCode}`);
     if (response.status === 404) return left(NotFound);
+    if (response.status === 400) return left(Error);
+    if (response.status === 500) return left(Error);
     if (!response.ok) return left(Error);
     const data = await response.json() as TrainStopTimes;
     return right(data);
