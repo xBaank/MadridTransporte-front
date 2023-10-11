55657b4a2491a0065ad73c5e40a9af16b3a609e5
Roberto Blázquez <elroby444@gmail.com>
2023-10-12 00:37:15 +0200

    Use service worker to cache

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     base: '/',
-    plugins: [react()]
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            injectRegister: 'auto',
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            }
+        })
+    ]
 })
