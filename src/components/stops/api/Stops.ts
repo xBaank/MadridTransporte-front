0306e53465f21406aa216f90e8454f344c79a065
Roberto Blázquez <elroby444@gmail.com>
2023-10-14 20:11:56 +0200

    Replace stops endpoint with static data

--- src/components/stops/api/Stops.ts
+++ src/components/stops/api/Stops.ts
@@ -3,27 +3,33 @@ import { Alert, Stop, TransportType } from "./Types";
 import { apiUrl } from "../../Urls";
 import { getCodModeByType } from "./Utils";
 
-let allStops: Either<string, Stop[]> | undefined
+let allStops: Either<string, Stop[]> | undefined;
 
 export async function getAllStops(): Promise<Either<string, Stop[]>> {
-    if (allStops !== undefined) return allStops;
-    const response = await fetch(`${apiUrl}/stops/all`);
-    if (!response.ok) return left("Error al obtener las paradas");
-    const data = await response.json() as Stop[];
-    allStops = right(data);
-    return right(data);
+  if (allStops !== undefined) return allStops;
+  const response = await fetch(`/stops/stops.json`);
+  if (!response.ok) return left("Error al obtener las paradas");
+  const data = (await response.json()) as Stop[];
+  allStops = right(data);
+  return right(data);
 }
 
 export async function getStop(type: TransportType, code: string) {
-    return fold(
-        () => null,
-        (stops: Stop[]) => stops.find(stop => stop.stop_code === code && stop.cod_mode == getCodModeByType(type))
-    )(await getAllStops());
+  return fold(
+    () => null,
+    (stops: Stop[]) =>
+      stops.find(
+        (stop) =>
+          stop.stop_code === code && stop.cod_mode == getCodModeByType(type)
+      )
+  )(await getAllStops());
 }
 
-export async function getAlertsByTransportType(type: TransportType): Promise<Either<string, Alert[]>> {
-    const response = await fetch(`${apiUrl}/stops/${type}/alerts`);
-    if (!response.ok) return left("Error al obtener las paradas");
-    const data = await response.json() as Alert[];
-    return right(data);
+export async function getAlertsByTransportType(
+  type: TransportType
+): Promise<Either<string, Alert[]>> {
+  const response = await fetch(`${apiUrl}/stops/${type}/alerts`);
+  if (!response.ok) return left("Error al obtener las paradas");
+  const data = (await response.json()) as Alert[];
+  return right(data);
 }
