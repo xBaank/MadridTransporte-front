376de31ce5fd558a4c6646ce05eb879652ace36a
Roberto Blázquez <elroby444@gmail.com>
2024-01-28 01:03:51 +0100

    Fix import

--- src/components/stops/train/TrainStopsTimes.tsx
+++ src/components/stops/train/TrainStopsTimes.tsx
@@ -21,7 +21,6 @@ import LoadingSpinner from "../../LoadingSpinner";
 import RenderAffected from "../Affected";
 import ErrorMessage from "../../Error";
 import StaledMessage from "../../Staled";
-import {interstitial} from "../../../admob";
 
 export default function TrainStopTimesComponent() {
   const [searchParams] = useSearchParams();
@@ -35,10 +34,6 @@ export default function TrainStopTimesComponent() {
   const textColor = theme.palette.mode === "dark" ? "text-white" : "text-black";
   const borderColor =
     theme.palette.mode === "dark" ? "border-white" : "border-black";
-
-  /*   useEffect(() => {
-    interstitial();
-  }, []); */
 
   useEffect(() => {
     if (origin === null || destination === null) {
