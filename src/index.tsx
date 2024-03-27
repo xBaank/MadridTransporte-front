cd795626a57c30802dbaff7fe522719569bbefee
Roberto Blázquez <elroby444@gmail.com>
2024-03-27 11:12:27 +0100

    Remove status bar on capacitor

--- src/index.tsx
+++ src/index.tsx
@@ -156,7 +156,6 @@ export const router = createBrowserRouter([
   },
 ]);
 
-showStatusBar();
 setupBackButton();
 
 let container: HTMLElement | null = null;
--- src/statusbar.ts
+++
@@ -1,8 +0,0 @@
-import {Capacitor} from "@capacitor/core";
-import {StatusBar} from "@capacitor/status-bar";
-
-export async function showStatusBar() {
-  if (Capacitor.getPlatform() === "web") return;
-  await StatusBar.setBackgroundColor({color: "#1e3a8a"});
-  await StatusBar.show();
-}
